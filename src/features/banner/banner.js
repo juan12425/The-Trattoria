bff96190ef91218af50213c000799521a203fc72
Juan <juanangelramirez1@hotmail.com>
2022-09-21 09:46:36 -0500

    First Commit

---
+++ src/features/banner/banner.js
@@ -0,0 +1,10 @@
+import "./banner.css";
+
+export function Banner()
+{
+    return(<div id="banner-div">
+        <div id="div-banner-title">
+            <h1>The Trattoria</h1>
+        </div>
+    </div>);
+}
---
+++ src/features/navbar/navbar.js
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+export function NavBar(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Book a Table</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
