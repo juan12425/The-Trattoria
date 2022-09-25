5e811e4a705b931841039a02ec0041af792c1133
Juan <juanangelramirez1@hotmail.com>
2022-09-24 21:49:15 -0500

    Cart link fixed

--- src/features/cart/cart.js
+++ src/features/cart/cart.js
@@ -7,6 +7,6 @@ export function Cart()
     const dishes=useSelector(selectAllDishes);
     const totalCount=getTotalCount(dishes);
     return(
-        <li><a href="#"><img id="cart-img" src={require("../../images/sp-cart.png")}/><span>{totalCount}</span></a></li>
+        <li><a href="#order-div"><img id="cart-img" src={require("../../images/sp-cart.png")}/><span>{totalCount}</span></a></li>
     );
 }
